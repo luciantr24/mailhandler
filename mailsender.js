947f60ba22b85de828fea4edf5ab74d96cea9a47
Lucian Traistaru <47867053+luciantr24@users.noreply.github.com>
2021-03-27 15:34:00 +0200

    Add files via upload

---
+++ mailsender.js
@@ -0,0 +1,28 @@
+async function SendEmail(username, password, req){
+    return new Promise((resolve, reject) =>{
+        nodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: username,
+                pass: password
+            } 
+        })
+
+        var mailOptions = {
+            from : req.body.email.from,
+            to: req.body.email.to,
+            subject: req.body.email.subject, 
+            text: req.body.email.text
+        }
+    
+        transporter.sendMail(mailOptions, (err, info) =>{
+            if(err) console.log(err);
+            console.log('Email sent: ' + info.response);
+        });
+
+    });
+}
+
+export default{
+    SendEmail
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+var express = require('express');
+var nodeMailer = require('nodemailer');
+import MailHandler from './mailsender';
+
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8080;
+
+const username = process.env.username;
+const password = process.env.password;
+
+var app = new express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/sendEmail', (req, res) =>{
+    MailHandler.SendEmail(username, password, req)
+        .then(res => res.send("Message sent"))
+        .catch(ex => console.log(ex))
+});
+
+var server = app.listen(PORT, () =>{
+    console.log("The server is running on port: " + PORT);
+});
