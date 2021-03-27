5864a476a1932e2f4eeab36599ce87e87bafd3f7
Lucian Traistaru <traistarulucian24@gmail.com>
2021-03-27 16:59:26 +0200

    added check if body is null

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ var app = new express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/sendEmail', (req, res) =>{
+app.post('/sendEmail', (req, res) => {
+    if(res.body == null)
+        return res.Send("Body cannot be null");
+
     MailHandler.SendEmail(username, password, req)
         .then(res => res.send("Message sent"))
         .catch(ex => console.log(ex))
